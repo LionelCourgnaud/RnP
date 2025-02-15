11aee6b404e625688de384ab39def27a52d00136
LionelCe <lionel.courgnaud@neuf.fr>
2025-02-15 17:53:52 +0100

    HBHelpers : classes clean

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -7,8 +7,8 @@ export const registerHandlebarsHelpers = () => {
     }); 
     // Include (array)
     Handlebars.registerHelper('inside', function(v1, v2) {
-        console.table(v1);
-        console.log(v1.includes(v2.toString()));
+        // console.table(v1);
+        // console.log(v1.includes(v2.toString()));
         return v1.includes(v2.toString());
     }); 
 };
