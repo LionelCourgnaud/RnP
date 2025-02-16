3f0fc6ba65cc23978b98a7a5359fa5d4bc718547
LionelCe <lionel.courgnaud@neuf.fr>
2025-02-16 22:02:51 +0100

    refacto helpers (eq)

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,16 +5,9 @@ export const registerHandlebarsHelpers = () => {
         console.log("IN");
         let nbValues = v2.length-1;
         let result = false;
-        let interat = 0;
         for(let i=0;i<nbValues;i++) {
-            console.log("v1 " + v1 + " / v2 "+v2[i]);
             result ||= v2[i] == v1;
-            console.log(v2[i] == v1);
         }
-        console.log("V1 "+v1);
-        console.log("V2 "+v2);
-        console.log(result);
-        console.log("OUT");
         return result;
     }); 
     // Include (array)
