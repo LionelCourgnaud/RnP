2d0994bdb73aee9f1f9d9e126ce1eb743f5bb2e7
LionelCe <lionel.courgnaud@neuf.fr>
2025-02-26 14:01:28 +0100

    (W) - ajout du .sh

---
+++ dev_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run the Foundry VTT app with the dev data path
+'/Applications/FoundryVTT-Dev/Foundry Virtual Tabletop.app/Contents/MacOS/Foundry Virtual Tabletop' --dataPath="/Users/lionel/Library/Application Support/FoundryVTT_dev"
